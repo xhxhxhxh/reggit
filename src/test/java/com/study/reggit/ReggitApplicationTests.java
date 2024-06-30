4d7a403100d81b45fc5bfb2ee99ddbf95f04184d
XH2 <546151373@qq.com>
2024-06-30 11:42:22 +0800

    项目初始化

---
+++ src/test/java/com/study/reggit/ReggitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.reggit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReggitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
