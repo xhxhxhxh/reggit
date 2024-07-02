7d857536fbf65cd95e7567bff76cfe3ff50d224e
xuhao <xuhao@mbse.com.cn>
2024-07-02 14:46:58 +0800

    员工列表

---
+++ src/main/java/com/study/reggit/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.study.reggit.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置分页插件
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+  @Bean
+  public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+    interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+    return interceptor;
+  }
+}
--- src/main/java/com/study/reggit/controller/EmployeeController.java
+++ src/main/java/com/study/reggit/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.study.reggit.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.study.reggit.common.R;
 import com.study.reggit.entity.Employee;
 import com.study.reggit.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -61,6 +60,12 @@ public class EmployeeController {
     return R.success("");
   }
 
+  /**
+   * 添加用户
+   * @param request
+   * @param employee
+   * @return
+   */
   @PostMapping
   public R<String> add(HttpServletRequest request, @RequestBody Employee employee) {
     employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
@@ -76,5 +81,26 @@ public class EmployeeController {
     employeeService.save(employee);
 
     return R.success("新增用户成功");
+  }
+
+  /**
+   * 员工列表分页查询
+   * @param page
+   * @param pageSize
+   * @param name
+   * @return
+   */
+  @GetMapping("/page")
+  public R<Page> userListPage(int page, int pageSize, String name) {
+    Page pageInfo = new Page(page, pageSize);
+
+    LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+    queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+
+    queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+    employeeService.page(pageInfo, queryWrapper);
+
+    return R.success(pageInfo);
   }
 }
